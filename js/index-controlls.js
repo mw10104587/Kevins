a33be203aea31a054357a6090f543d16a486aba7
mw10104587 <mw10104587@gmail.com>
2015-10-11 01:18:39 -0400

    add dropdown function

---
+++ js/index-controlls.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function(){
+
+
+	$(window).scroll(function(){
+				var st = $(this).scrollTop();
+				// console.log($(this).scrollTop());
+
+				var targetH = 546;
+
+				if ( st > targetH ) {
+					$("#filters-container").addClass("fixed");
+					// $("#map").css({marginTop: anchorHeight}).after($("#fake-scroll"));
+				}else if( st < targetH){
+					$("#filters-container").removeClass("fixed");
+				}else{
+					console.log("hello");
+				}
+			});
+
+
+
+
+	// drop down stuff
+	$(".filter").find(".dropdown-menu").find("li").click(function(){
+
+		var newText = $(this).find("a").text();
+		console.log(newText);
+
+		$(this).closest(".filter").find("button").html(newText+'<span class="caret"></span>');
+
+		console.log($(this).closest(".filter-drop-down"));
+
+	});
+
+
+
+})
+
